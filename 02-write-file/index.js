bb8ef4e56eba39ab7ccff1fc98c832f266e28cd9
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-01-16 13:32:02 +0600

    feat: complete 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+const fs = require('node:fs');
+const path = require('path');
+
+const writeStream = fs.createWriteStream(path.join(__dirname, './text.txt'), {
+  flags: 'w',
+});
+const rl = readline.createInterface({ input, output });
+
+console.log('Hello! Start typing. Type "exit" to quit:');
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Good bye!');
+    rl.close();
+  } else {
+    writeStream.write(input + '\n');
+  }
+});
+
+rl.on('close', () => {
+  writeStream.end();
+});
+
+rl.on('SIGINT', () => {
+  console.log('Good bye!');
+  rl.close();
+});
